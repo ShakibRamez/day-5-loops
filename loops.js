eb5f75ff8c3c87ad5b00bde7569b4458b4be52b8
ShakibRamez <shakibramiz1@icloud.com>
2025-06-05 11:14:54 +0430

    loops do while for

---
+++ loops.js
@@ -0,0 +1,26 @@
+//loops
+// //for loops
+
+// const names = ["john", "bob", "shakib", "ramez"];
+
+// // for(let i = 0; i < 5; i++){
+// //     console.log("loops");
+// //     if(i === 3) break;
+// // }
+
+// // for(name of names) console.log(name);
+
+// const user = ["firstName", "john", "lastName", "ramez"];
+// for(key in user)console.log(user[key]);
+
+
+//do/while loops
+
+// let i = 0;
+// do{
+//      console.log(i);
+//     i++;
+//     if(i === 5) continue;
+//     console.log(i);
+// }
+// while(i < 0);
